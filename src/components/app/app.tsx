9e463bbbabb2d919440cb74f4ec1b5ce5d8d4d78
Kol1n13 <kolin.1312@yandex.ru>
2024-10-16 16:44:03 +0500

    task3

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,24 @@
 import { MainPage } from "../../pages/main/main";
 import { SimpleCard } from "../../types/place-card-type";
+import { Error404 } from "../../pages/error404/error404";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import { AppRoute } from "../../route-types";
+import { PrivateRoutes } from "../private-routes/private-routes";
 
 type warp = {
     availableCards : SimpleCard[]
 }
 
 export function App({availableCards} : warp){
+    let isUserAuthorized = false;
+    let authorizedRoutes = PrivateRoutes({isAuthorized : isUserAuthorized})
     return (
-        MainPage(availableCards)
+        <BrowserRouter>
+            <Routes>
+                <Route path={AppRoute.Main} element={<MainPage availableCards={availableCards}/>}/>
+                <Route path={AppRoute.Error404} element={<Error404 />}/>
+                {authorizedRoutes}
+            </Routes>
+        </BrowserRouter>
     )
 }
