9f7d3f5b74e93a13a6ba0fa2c37bcf51bc5d2ef6
Kol1n13 <kolin.1312@yandex.ru>
2024-11-09 12:19:06 +0500

    5-1

--- src/mocks/detailed-offer.ts
+++ src/mocks/detailed-offer.ts
@@ -1,200 +1,198 @@
 import { FullOfferType } from "../types/offer-full-description-type";
 
 export const fullOffers : FullOfferType[] = [
-    {
-        "id": "3dfb4c8d-7444-46a4-8f5b-ce80ad0cffbf",
-        "title": "Perfectly located Castro",
-        "description": "I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.",
-        "type": "apartment",
-        "price": 294,
-        "images": [
-          "https://14.design.htmlacademy.pro/static/hotel/16.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/13.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/9.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/4.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/10.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/8.jpg"
-        ],
-        "city": {
-          "name": "Paris",
-          "location": {
-            "latitude": 48.85661,
-            "longitude": 2.351499,
-            "zoom": 13
-          }
-        },
-        "location": {
-          "latitude": 48.86861,
-          "longitude": 2.342499,
-          "zoom": 16
-        },
-        "goods": [
-          "Air conditioning",
-          "Washing machine",
-          "Fridge",
-          "Towels",
-          "Coffee machine",
-          "Wi-Fi",
-          "Dishwasher",
-          "Heating",
-          "Baby seat",
-          "Cable TV",
-          "Breakfast"
-        ],
-        "host": {
-          "isPro": true,
-          "name": "Angelina",
-          "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
-        },
-        "isPremium": false,
-        "isFavorite": false,
-        "rating": 2.6,
-        "bedrooms": 5,
-        "maxAdults": 9
+  {
+    "id": "337fc635-709d-4c54-9fed-3a20f68f67ef",
+    "title": "Penthouse, 4-5 rooms + 5 balconies",
+    "description": "A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.",
+    "type": "room",
+    "price": 248,
+    "images": [
+      "https://14.design.htmlacademy.pro/static/hotel/10.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/7.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/12.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/1.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/2.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/3.jpg"
+    ],
+    "city": {
+      "name": "Amsterdam",
+      "location": {
+        "latitude": 52.37454,
+        "longitude": 4.897976,
+        "zoom": 13
+      }
     },
-    {
-        "id": "3153c353-38b8-4e54-b689-2df57295f450",
-        "title": "Nice, cozy, warm big bed apartment",
-        "description": "I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.",
-        "type": "apartment",
-        "price": 428,
-        "images": [
-          "https://14.design.htmlacademy.pro/static/hotel/9.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/1.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/16.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/11.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/4.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/10.jpg"
-        ],
-        "city": {
-          "name": "Paris",
-          "location": {
-            "latitude": 48.85661,
-            "longitude": 2.351499,
-            "zoom": 13
-          }
-        },
-        "location": {
-          "latitude": 48.83461,
-          "longitude": 2.335499,
-          "zoom": 16
-        },
-        "goods": [
-          "Air conditioning",
-          "Washing machine",
-          "Kitchen",
-          "Wi-Fi",
-          "Washer",
-          "Breakfast",
-          "Cable TV",
-          "Towels"
-        ],
-        "host": {
-          "isPro": true,
-          "name": "Angelina",
-          "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
-        },
-        "isPremium": false,
-        "isFavorite": false,
-        "rating": 1.1,
-        "bedrooms": 3,
-        "maxAdults": 6
+    "location": {
+      "latitude": 52.36554,
+      "longitude": 4.911976,
+      "zoom": 16
     },
-    {
-        "id": "2405a442-7189-4ba5-86af-3a71ce85c813",
-        "title": "Wood and stone place",
-        "description": "Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.",
-        "type": "room",
-        "price": 258,
-        "images": [
-          "https://14.design.htmlacademy.pro/static/hotel/18.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/20.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/17.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/6.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/13.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/8.jpg"
-        ],
-        "city": {
-          "name": "Paris",
-          "location": {
-            "latitude": 48.85661,
-            "longitude": 2.351499,
-            "zoom": 13
-          }
-        },
-        "location": {
-          "latitude": 48.87561,
-          "longitude": 2.375499,
-          "zoom": 16
-        },
-        "goods": [
-          "Baby seat",
-          "Wi-Fi",
-          "Laptop friendly workspace",
-          "Breakfast",
-          "Washer",
-          "Heating",
-          "Kitchen",
-          "Air conditioning",
-          "Towels",
-          "Fridge",
-          "Dishwasher"
-        ],
-        "host": {
-          "isPro": true,
-          "name": "Angelina",
-          "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
-        },
-        "isPremium": true,
-        "isFavorite": false,
-        "rating": 1.8,
-        "bedrooms": 1,
-        "maxAdults": 2
+    "goods": [
+      "Heating",
+      "Coffee machine",
+      "Breakfast",
+      "Dishwasher",
+      "Kitchen",
+      "Baby seat",
+      "Fridge"
+    ],
+    "host": {
+      "isPro": true,
+      "name": "Angelina",
+      "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
     },
-    {
-        "id": "e72fa761-c6f0-4ed3-8ace-5b39ade314f4",
-        "title": "The house among olive ",
-        "description": "Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.",
-        "type": "hotel",
-        "price": 226,
-        "images": [
-          "https://14.design.htmlacademy.pro/static/hotel/17.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/9.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/16.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/1.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/4.jpg",
-          "https://14.design.htmlacademy.pro/static/hotel/7.jpg"
-        ],
-        "city": {
-          "name": "Paris",
-          "location": {
-            "latitude": 48.85661,
-            "longitude": 2.351499,
-            "zoom": 13
-          }
-        },
-        "location": {
-          "latitude": 48.86161,
-          "longitude": 2.340499,
-          "zoom": 16
-        },
-        "goods": [
-          "Kitchen",
-          "Washer",
-          "Dishwasher",
-          "Washing machine",
-          "Heating"
-        ],
-        "host": {
-          "isPro": true,
-          "name": "Angelina",
-          "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
-        },
-        "isPremium": true,
-        "isFavorite": false,
-        "rating": 1.9,
-        "bedrooms": 1,
-        "maxAdults": 3
-    }
+    "isPremium": true,
+    "isFavorite": false,
+    "rating": 1.6,
+    "bedrooms": 1,
+    "maxAdults": 1
+  },
+  {
+    "id": "fb768e57-b767-41a2-909f-f950f2fd6855",
+    "title": "Loft Studio in the Central Area",
+    "description": "Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.",
+    "type": "room",
+    "price": 118,
+    "images": [
+      "https://14.design.htmlacademy.pro/static/hotel/10.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/18.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/13.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/16.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/20.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/15.jpg"
+    ],
+    "city": {
+      "name": "Amsterdam",
+      "location": {
+        "latitude": 52.37454,
+        "longitude": 4.897976,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 52.385540000000006,
+      "longitude": 4.902976,
+      "zoom": 16
+    },
+    "goods": [
+      "Washing machine",
+      "Wi-Fi",
+      "Baby seat",
+      "Fridge",
+      "Laptop friendly workspace",
+      "Towels",
+      "Air conditioning"
+    ],
+    "host": {
+      "isPro": true,
+      "name": "Angelina",
+      "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
+    },
+    "isPremium": false,
+    "isFavorite": false,
+    "rating": 1.7,
+    "bedrooms": 1,
+    "maxAdults": 1
+  },
+  {
+    "id": "6fb62ba1-b15e-4022-a1fc-90d3872e1d32",
+    "title": "Tile House",
+    "description": "Design interior in most sympathetic area! Complitely renovated, well-equipped, cosy studio in idyllic, over 100 years old wooden house. Calm street, fast connection to center and airport.",
+    "type": "house",
+    "price": 474,
+    "images": [
+      "https://14.design.htmlacademy.pro/static/hotel/9.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/1.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/7.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/10.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/19.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/11.jpg"
+    ],
+    "city": {
+      "name": "Amsterdam",
+      "location": {
+        "latitude": 52.37454,
+        "longitude": 4.897976,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 52.397540000000006,
+      "longitude": 4.9099759999999995,
+      "zoom": 16
+    },
+    "goods": [
+      "Kitchen",
+      "Air conditioning",
+      "Laptop friendly workspace",
+      "Breakfast",
+      "Wi-Fi",
+      "Fridge",
+      "Heating",
+      "Dishwasher",
+      "Cable TV"
+    ],
+    "host": {
+      "isPro": true,
+      "name": "Angelina",
+      "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
+    },
+    "isPremium": false,
+    "isFavorite": false,
+    "rating": 1.8,
+    "bedrooms": 4,
+    "maxAdults": 7
+  },
+  {
+    "id": "2a31dd82-5ae8-465e-b836-55fad5c9b75b",
+    "title": "Beautiful & luxurious apartment at great location",
+    "description": "I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.",
+    "type": "apartment",
+    "price": 177,
+    "images": [
+      "https://14.design.htmlacademy.pro/static/hotel/8.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/20.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/3.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/11.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/13.jpg",
+      "https://14.design.htmlacademy.pro/static/hotel/4.jpg"
+    ],
+    "city": {
+      "name": "Amsterdam",
+      "location": {
+        "latitude": 52.37454,
+        "longitude": 4.897976,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 52.37454,
+      "longitude": 4.881976,
+      "zoom": 16
+    },
+    "goods": [
+      "Laptop friendly workspace",
+      "Washing machine",
+      "Dishwasher",
+      "Wi-Fi",
+      "Coffee machine",
+      "Breakfast",
+      "Fridge",
+      "Washer",
+      "Air conditioning",
+      "Baby seat"
+    ],
+    "host": {
+      "isPro": true,
+      "name": "Angelina",
+      "avatarUrl": "https://14.design.htmlacademy.pro/static/host/avatar-angelina.jpg"
+    },
+    "isPremium": true,
+    "isFavorite": false,
+    "rating": 3.4,
+    "bedrooms": 5,
+    "maxAdults": 9
+  }
 
 ]
